560e12bda5563f441e4c9ffcf72f77dfe05e788b
Martijn Jacobs <maerteijn@gmail.com>
2020-03-15 22:45:45 +0100

    Added a test to see if we export the modules we want

---
+++ tests/test_index.js
@@ -0,0 +1,13 @@
+import { assert } from "chai"
+import BandItJS from "../src/index"
+
+describe("Export modules", () => {
+  it("We should export all modules available in this package", () => {
+    assert("BandItChordProFormatter" in BandItJS)
+    assert("BandItChordSheetParser" in BandItJS)
+    assert("parseChordSheet" in BandItJS)
+    assert("parseChordPro" in BandItJS)
+    assert("dumpSongToChordPro" in BandItJS)
+    assert("validator" in BandItJS)
+  })
+})
