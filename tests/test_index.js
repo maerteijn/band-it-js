4bd09641ed972031fc0ae672500f5bab354321d6
Martijn Jacobs <maerteijn@gmail.com>
2020-03-16 15:18:23 +0100

    Test that index.js exports BandItSong as well

--- tests/test_index.js
+++ tests/test_index.js
@@ -5,6 +5,7 @@ describe("Export modules", () => {
   it("We should export all modules available in this package", () => {
     assert("BandItChordProFormatter" in BandItJS)
     assert("BandItChordSheetParser" in BandItJS)
+    assert("BandItSong" in BandItJS)
     assert("parseChordSheet" in BandItJS)
     assert("parseChordPro" in BandItJS)
     assert("dumpSongToChordPro" in BandItJS)
