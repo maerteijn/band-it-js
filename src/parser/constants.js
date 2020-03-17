5291ae25dbd2dff33b162a319e7fc688bd4c7097
Martijn Jacobs <maerteijn@gmail.com>
2020-03-17 22:55:05 +0100

    Added a parser constants file

---
+++ src/parser/constants.js
@@ -0,0 +1,17 @@
+export const GRID = "grid"
+export const START_OF_GRID = "start_of_grid"
+export const END_OF_GRID = "end_of_grid"
+export const GRID_REGEX = /(grid)(?<value>.*)/i
+
+export const SECTION = "x_section"
+export const START_OF_SECTION = "x_start_of_section"
+export const END_OF_SECTION = "x_end_of_section"
+
+export const HEADER_REGEX = /\[(?<value>.*)\]/
+export const VERSE_CHORUS_REGEX = /(?<section>verse|chorus)(.*)/i
+export const CHORUS_REGEX = /(chorus)(.*)/i
+
+export const TITLE_ARTIST_REGEX = /(?<title>.*)-(?<artist>.*)/
+export const META_TAG_REGEX = /(?<key>.*):(?<value>.*)/
+
+export const CONTROL_CHARS = "{"
