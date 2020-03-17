12b01c4d173f0757d0042eee39e37d52e52f80d3
Martijn Jacobs <maerteijn@gmail.com>
2020-03-17 23:14:47 +0100

    Also export the BandItChordProParser

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import { BandItChordProFormatter } from "./formatter"
 import { BandItChordSheetParser } from "./parser/chordsheet"
+import { BandItChordProParser } from "./parser/chordpro"
+import { BandItChordProFormatter } from "./formatter"
 import { BandItSong } from "./song"
 import { parseChordSheet, parseChordPro, dumpSongToChordPro } from "./utils"
 import { validator } from "./schema"
 
 export default {
-  BandItChordProFormatter,
   BandItChordSheetParser,
+  BandItChordProParser,
+  BandItChordProFormatter,
   BandItSong,
   parseChordSheet,
   parseChordPro,
--- tests/test_index.js
+++ tests/test_index.js
@@ -3,8 +3,9 @@ import BandItJS from "../src/index"
 
 describe("Export modules", () => {
   it("We should export all modules available in this package", () => {
-    assert("BandItChordProFormatter" in BandItJS)
     assert("BandItChordSheetParser" in BandItJS)
+    assert("BandItChordProParser" in BandItJS)
+    assert("BandItChordProFormatter" in BandItJS)
     assert("BandItSong" in BandItJS)
     assert("parseChordSheet" in BandItJS)
     assert("parseChordPro" in BandItJS)
